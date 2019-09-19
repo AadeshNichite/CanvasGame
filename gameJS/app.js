5747edd82ad8aa9c40fe05cbfbda32291af1e478
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-09-19 12:10:15 +0530

    version-1
    updated

---
+++ gameJS/app.js
@@ -0,0 +1,14 @@
+class player {
+    constructor(player1,player2) {
+      this.player1 = player1;
+      this.player2 = player2;
+      console.log(this.player1.value,this.player2.value)
+    }
+  }
+
+  function init()
+  {
+    var player1=document.getElementById('player1');
+    var player2=document.getElementById('player2');
+    player = new player(player1,player2);
+  }
