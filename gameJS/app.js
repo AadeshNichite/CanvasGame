96b8be2bf71fb5f0934eaca1d7c478ff4056a6ec
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-09-19 12:42:15 +0530

    updates

--- gameJS/app.js
+++ gameJS/app.js
@@ -1,6 +1,27 @@
 // GoblinGo Game
 // Author: Aadesh Nichite
 // It is a simple game application
+// class game{
+//     constructor(canvas,width, height){
+
+//         this.canvas = canvas;
+
+//         this.width = width;
+
+//         this.height = height;
+
+//         canvas.width = width;
+
+//         canvas.height = height;
+
+//         this.ctx = canvas.getContext('2d');
+
+
+//         var img = document.getElementById("playGround");
+//         ctx.drawImage(img, 10, 10);
+
+//      }
+//   }
 
 class player {
     constructor(player1,player2) {
@@ -12,7 +33,12 @@ class player {
 
   function init()
   {
-    var player1=document.getElementById('player1');
-    var player2=document.getElementById('player2');
+    let player1=document.getElementById('player1');
+    let player2=document.getElementById('player2');
     player = new player(player1,player2);
+    // let canvas = document.getElementById('game');
+    // let game = new Game(canvas,600,380);
+    
   }
+
+  
